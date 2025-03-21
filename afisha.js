aa8b4970c255fde0abb348c1b40b29d804c55066
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-03-21 13:59:58 +0200

    fixed js

--- afisha.js
+++ afisha.js
@@ -1,9 +1,29 @@
-document.getElementById('download-btn').addEventListener('click', function () {
-  const poster = document.querySelector('.poster-container');
-  html2canvas(poster).then(function (canvas) {
-    const link = document.createElement('a');
-    link.href = canvas.toDataURL();
-    link.download = 'afisha.png';
-    link.click();
+window.onload = function () {
+  const downloadBtn = document.getElementById('download-btn');
+
+  downloadBtn.addEventListener('click', function () {
+    const poster = document.querySelector('.poster-container');
+
+    if (poster) {
+
+      html2canvas(poster, {
+        logging: true, 
+        useCORS: true, 
+        letterRendering: true, 
+        scale: 2,
+      })
+        .then(function (canvas) {
+ 
+          const link = document.createElement('a');
+          link.href = canvas.toDataURL();
+          link.download = 'afisha.png';
+          link.click();
+        })
+        .catch(function (error) {
+          console.error('Помилка при створенні зображення:', error);
+        });
+    } else {
+      console.error('Елемент з класом .poster-container не знайдено.');
+    }
   });
-});
+};
